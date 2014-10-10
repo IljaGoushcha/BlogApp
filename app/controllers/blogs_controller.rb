6165416b7e539ccbb4baa24903a3e2ea88314adb
IljaGoushcha <iljagou@yahoo.com>
2014-10-10 00:10:45 -0400

    added create blog functionality

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,4 +4,19 @@ class BlogsController < ApplicationController
     @blogs = Blog.all
   end
 
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(allowed_params)
+    @blog.save
+    redirect_to blogs_path
+  end
+
+  private
+
+  def allowed_params
+    params.require(:blog).permit(:title, :body, :user_name)
+  end
 end
